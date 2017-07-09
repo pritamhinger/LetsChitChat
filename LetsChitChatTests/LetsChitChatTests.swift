f7c5dc10ceba5307ce1003af6e08f8b47eb5af54
pritam hinger <pritamgenius24@gmail.com>
2017-07-09 16:20:24 +0530

    Initial Commit

---
+++ LetsChitChatTests/LetsChitChatTests.swift
@@ -0,0 +1,36 @@
+//
+//  LetsChitChatTests.swift
+//  LetsChitChatTests
+//
+//  Created by Pritam Hinger on 09/07/17.
+//  Copyright © 2017 AppDevelapp. All rights reserved.
+//
+
+import XCTest
+@testable import LetsChitChat
+
+class LetsChitChatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
