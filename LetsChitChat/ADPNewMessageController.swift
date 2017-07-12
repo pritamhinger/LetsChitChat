91ca7795a5ced0321bb566789efe843b5db5895c
pritam hinger <pritamgenius24@gmail.com>
2017-07-12 10:33:17 +0530

    CodeIn Logic to download profile images of all the users.
    CodeIn logic to download images using download url.
    Write Custom NewMessageCell class to contain a customized UIImageView into the cell.
    Write UIImageView extension to better manage image download feature.
    Use NSCache to cache downloaded images.

--- LetsChitChat/ADPNewMessageController.swift
+++ LetsChitChat/ADPNewMessageController.swift
@@ -51,21 +51,56 @@ class ADPNewMessageController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! NewMessageCell
         let user = users[indexPath.row]
         cell.textLabel?.text = user.name!
         cell.detailTextLabel?.text = user.email!
+        
+        
+        if let profileImageUrl = user.profileImageURL{
+            cell.profileImageView.loadImageFromCache(withUrl: profileImageUrl)
+        }
+        
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
     }
 }
 
 
 class NewMessageCell : UITableViewCell{
+    
+    let profileImageView:UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "chitchat_logo")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 24
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 48).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("Not implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        textLabel?.frame = CGRect(x: 64, y: textLabel!.frame.origin.y - 2, width: textLabel!.frame.width, height: textLabel!.frame.height)
+        detailTextLabel?.frame = CGRect(x: 64, y: detailTextLabel!.frame.origin.y + 2, width: detailTextLabel!.frame.width, height: detailTextLabel!.frame.height)
     }
 }
--- LetsChitChat/Extensions.swift
+++ LetsChitChat/Extensions.swift
@@ -6,11 +6,39 @@
 //  Copyright © 2017 AppDevelapp. All rights reserved.
 //
 
-import Foundation
+//import Foundation
 import UIKit
+
+let imageCahce = NSCache<AnyObject, AnyObject>()
 
 extension UIColor{
     convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
         self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+}
+
+extension UIImageView{
+    func loadImageFromCache(withUrl urlString:String) {
+        self.image = nil
+        if let cachedImage = imageCahce.object(forKey: urlString as AnyObject) as? UIImage{
+            self.image = cachedImage
+            return
+        }
+        
+        let url = URL(string: urlString)
+        URLSession.shared.dataTask(with: url!, completionHandler: {(data, response, error) in
+            if error != nil{
+                print(error!)
+            }
+            else{
+                print("setting image")
+                DispatchQueue.main.async {
+                    if let downloadedImage = UIImage(data: data!){
+                        imageCahce.setObject(downloadedImage, forKey: urlString as AnyObject)
+                        self.image = downloadedImage
+                    }
+                }
+            }
+        }).resume()
     }
 }
